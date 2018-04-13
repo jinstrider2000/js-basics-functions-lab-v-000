f3e74fc23e096f4d6df44e7b5968839e2d1d8159
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 15:43:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  return Math.abs(streetNumber - 42);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+  return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(startStreet,endStreet) {
+  return Math.abs(startStreet - endStreet) * 264;
+}
