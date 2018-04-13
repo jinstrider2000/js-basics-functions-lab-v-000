2b82de227e50dc1d21439629a676032c7258dae3
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 15:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ function distanceFromHqInFeet(streetNumber) {
   return distanceFromHqInBlocks(streetNumber) * 264;
 }
 
-function distanceTravelledInFeet(startStreet,endStreet) {
+function distanceTravelledInFeet(startStreet, endStreet) {
   return Math.abs(startStreet - endStreet) * 264;
+}
+
+function calculatesFarePrice(startStreet, endStreet) {
+  const absDistance = distanceTravelledInFeet(startStreet, endStreet);
+  let farePrice;
+  
+  if (absDistance > 400)
+  {
+    farePrice = 0;  
+  }
+  else if (absDistance >= 400 && absDistance < 2000)
+  {
+    farePrice = .02 * absDistance;
+  }
+  else if (absDistance >= 2000 && absDistance < 2500)
+  {
+    farePrice = 25;
+  }
+  else
+  {
+    return 'Cannot travel that far'
+  }
 }
